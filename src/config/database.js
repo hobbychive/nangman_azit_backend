36cc4da3d99baa08ccbb90641449c2ebda7154d6
hobbychive <parksw123098@gmail.com>
2025-01-12 06:28:47 +0900

    database to mysql

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const mariadb = require("mariadb");
+const mysql = require("mysql2");
 
-const pool = mariadb.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
